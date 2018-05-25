a13ebed4091d3ad2f85f42df189ac451b45d720d
Bharath <sathuribharathbrothers1234@gmail.com>
2018-05-25 17:26:42 +0530

    nav bar logo with footer

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 80,
+        "value": 120,
         "density": {
           "enable": true,
           "value_area": 800
